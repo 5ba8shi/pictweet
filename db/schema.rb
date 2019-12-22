848c0067a1bfb89ae678293c568fdb5561c710d4
5ba8shi <55795442+5ba8shi@users.noreply.github.com>
2019-12-23 08:58:31 +0900

    up

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 ActiveRecord::Schema.define(version: 2019_12_17_135044) do
 
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "text"
+    t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
