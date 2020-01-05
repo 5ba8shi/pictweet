846eb910d6d8fb40bf9dc464bfbbe0fb420224ec
5ba8shi <55795442+5ba8shi@users.noreply.github.com>
2020-01-05 14:44:19 +0900

    up

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_30_043932) do
+ActiveRecord::Schema.define(version: 2020_01_05_053420) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
@@ -37,11 +37,8 @@ ActiveRecord::Schema.define(version: 2019_12_30_043932) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "nickname"
-    t.string "provider"
     t.string "uid"
-    t.string "name"
-    t.string "refresh_token"
-    t.string "access_token"
+    t.string "provider"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
