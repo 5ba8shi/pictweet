4bdbea4a6cd1c1e65b1bddf79fc25de79cfd1dbb
5ba8shi <55795442+5ba8shi@users.noreply.github.com>
2020-01-09 06:55:16 +0900

    up

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,4 +1,24 @@
 $(function(){
+
+  let search_list = $(".contents.row");
+
+  function appendTweet(tweet) {
+    let current_user = tweet.user_sign_in && tweet.user_sign_in.id == tweet.user_id ?
+
+    let current_user = tweet.user_sign_in && tweet.user_sign_in.id == tweet.user_id ?
+
+    <% if user_signed_in? && current_user.id == tweet.user_id %>
+    <li>
+      <%= link_to '編集', "/tweets/#{tweet.id}/edit", method: :get %>
+    </li>
+    <li>
+      <%= link_to '削除', "/tweets/#{tweet.id}", method: :delete %>
+    </li>
+
+
+  }
+
+
   $(".search-input").on("keyup", function() {
     let input = $(".search-input").val();
     $.ajax({
