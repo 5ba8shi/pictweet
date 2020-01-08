4985603b5a024b8f55a65e305442246da48af499
5ba8shi <55795442+5ba8shi@users.noreply.github.com>
2020-01-08 12:48:26 +0900

    up

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,4 +1,13 @@
 $(function(){
+  function buildHTML(comment){
+    let html =`<p>
+                  <strong>
+                    <a href=/users/${comment.user_id}>${comment.user_name}</a>
+                  </strong>
+                  ${comment.text}
+                </p>`
+    return html;
+  }
   $('#new_comment').on('submit', function(e){
     e.preventDefault();
     let formData = new FormData(this);
@@ -10,6 +19,12 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      let html = buildHTML(data);
+      $('.comments').append(html);
+      $('.textbox').val('');
+      $('.form__submit').prop('disabled', false);
     })    
   })
 })
